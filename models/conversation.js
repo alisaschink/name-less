5ced6ade0d41accea4f6b7aad98ce871a554ba8e
Alisa Schink <Alisa Schink>
2017-04-23 01:37:15 -0400

    models started

---
+++ models/conversation.js
@@ -0,0 +1,20 @@
+// module.exports = function(sequelize, DataTypes) {
+//   var Conversation = sequelize.define('User',
+//     // columns of table
+//     {
+//       is_employer: {
+//         type: DataTypes.BOOLEAN,
+//         defaultValue: false,
+//       }
+//     },
+//     // options
+//     {
+//       underscored: true,
+//       freezeTableName: true,
+//       classMethods: {
+//         associate: function(models) {
+//         }
+//       } // end of classMethods
+//     }); // end of .define
+//   return Conversation;
+// };
