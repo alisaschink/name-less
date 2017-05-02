15a189d30a6ff5de3d7127b6e52b074527a930ff
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2017-05-01 21:19:08 -0400

    adding create new job option for employers - not working 100% but going to incorporate new models now anyway

--- routes/JobController.js
+++ routes/JobController.js
@@ -24,4 +24,24 @@ router.post('/update', function(req,res){
     });
 });
 
+router.post('/new', function(req,res){
+	console.log(req.body)
+	var changes = {
+		title: req.body.title, 
+		location: req.body.location,
+		description: req.body.description,
+		responsibilities: req.body.responsibilities,
+		qualifications: req.body.qualifications
+		company_id: req.body.company_id,
+		// user_id: req.body.user_id,
+		industry_id: req.body.industry_id,
+	}
+	console.log(changes)
+	db.Credential.create(changes).then(function(result) {
+  	console.log(result)
+    res.redirect("/applicant/home")
+    });
+
+});
+
 module.exports = router;
